7dba4a8bc7c38845279af7b22ba7025ffcd17a52
Flapjacck <plkelly333@me.com>
2026-03-14 11:47:37 -0400

    Feat: graph class fields & constructor

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,23 @@
+
 /*
  * Simple directed weighted graph representation backed by an adjacency matrix.
  * Provides methods to read the matrix from standard input and to construct the
  * transposed graph (edges reversed) required by the policy logic.
  */
+import java.util.Scanner;
+
 class Graph {
-    // TODO: fields and constructors
+    int n;
+    int[][] adj;
+
+    Graph(int n) {
+        this.n = n;
+        this.adj = new int[n][n];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                adj[i][j] = sc.nextInt();
+            }
+        }
+    }
 }
